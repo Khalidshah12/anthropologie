2a89d318dd06385961dae94f55f53372c07de22a
sohilweb20 <sohailkhanhatewa@gmail.com>
2022-11-10 20:06:32 +0530

    added navbar footer and home screen

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -1,9 +1,40 @@
 import React from "react";
-
+import "./HOme.css";
 import Layout from "../../components/Layout";
 
 const Home = () => {
-  return <Layout>HOme </Layout>;
+  return (
+    <Layout>
+      <div className="FirstDiv">
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/66gBHQY9rlKtf5IItLdMCa/eb48274717871eb64ba75900e042218f/LS_M0_Banner_a.jpg?w=630&q=80&fm=webp"
+          alt=""
+        />
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/7FtJ5t3fiV8RM8Q80ufmqS/4b8d59935ecd871b8f0b1055665bbe09/LS_M0_Banner_b.jpg?w=630&q=80&fm=webp"
+          alt=""
+        />
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/6Rg6bbVw5EcY8ZFGylD3Lt/52e96081fac4772cbd0e5aabda58219a/LS_M0_Banner_c.jpg?w=630&q=80&fm=webp"
+          alt=""
+        />
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/2jP2GsJVfSqC66yrxJJPOX/a4b3ecfc7c2a6de75aa10b764c9cfe9f/LS_M0_Banner_d.jpg?w=630&q=80&fm=webp"
+          alt=""
+        />
+      </div>
+      <div className="SecondDiv">
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/iMM14RuhLCVUGYVmrKvc9/b97a6a1d371ca72bbc1ae9aaf4a11f26/LS_M1a.jpg?w=1302&q=80&fm=webp"
+          alt=""
+        />
+        <img
+          src="https://images.ctfassets.net/5de70he6op10/6pgHrENVmjeLfmWwwGZuiG/7087546a3b3529f4de0de0ccedca384e/LS_M1b.jpg?w=1302&q=80&fm=webp"
+          alt=""
+        />
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -82,8 +82,43 @@ const Navbar = () => {
       </div>
       <nav>
         <ul className="navbar">
-          <li>Gift</li>
-          <li>New</li>
+          <li className="dropdown">
+            Gift
+            <ul className="child-dropdown">
+              <a>Shop By Category</a>
+              <div className="hovergrid">
+                <div className="hovergrid1">
+                  <li>Explore All Gifts</li>
+                  <li>Shop All Gifts</li>
+                  <li>Gifts for Her</li>
+                  <li>Candle gift & sets</li>
+                  <li>Holiday ,decor </li>
+                  <li>Stocking suffers</li>
+                  <li>Giftable Sweaters</li>
+                  <li>Giftable Accessories</li>
+                  <li>Gifts for the home</li>
+                  <li>Cozy Gifts ,slippers,Throws</li>
+                  <li>Cold weather Gifts </li>
+                  <li>Beauty gifts and sets</li>
+                  <li>Gifts by Receipts</li>
+                  <li>BestSeling Gifts</li>
+                  <li>Gifts Bundles</li>
+                  <li>Gifts Cards</li>
+                </div>
+
+                <div className="hovergrid2">
+                  <h3>Gifts under price</h3>
+                  <div>
+                    <li>Gifts under 30$</li>
+                    <li>Gifts under 50$ </li>
+                    <li>Gifts under 25$</li>
+                    <li> Gifts under 100$</li>
+                  </div>
+                </div>
+              </div>
+            </ul>
+          </li>
+          <li>New!</li>
           <li>Dresses</li>
           <li>Clothing</li>
           <li>Shoes</li>

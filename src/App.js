b7caa6d6ac7f0dcf235e2bfba10417155f06b889
sohilweb20 <sohailkhanhatewa@gmail.com>
2022-11-12 21:13:36 +0530

    added finally version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 import Allroutes from "./Pages/Allroutes";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Allroutes />
+      <Footer />
     </div>
   );
 }
